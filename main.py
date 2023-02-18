d12321dc9f1af1ebeb3fba17dcacb8f5a62b367a
Sourcery AI <>
2023-02-18 15:48:07 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ print("Gen Started. Enjoy!")
 #gen codes
 def gencode():
     letters = string.ascii_letters + string.digits
-    return ''.join(random.choice(letters) for i in range(19))
+    return ''.join(random.choice(letters) for _ in range(19))
 
 class Generator:
     def __init__(self):
@@ -33,19 +33,16 @@ class Generator:
             code = gencode()
             self.codes.append(code)
             response = requests.get(
-                "https://discord.com/api/v7/entitlements/gift-codes/" + code + "?with_application=false&with_subscription_plan=true")
+                f"https://discord.com/api/v7/entitlements/gift-codes/{code}?with_application=false&with_subscription_plan=true"
+            )
             data = response.json()
             if data["message"] == 'Unknown Gift Code':
-                print("Invalid: " + code)
-            elif data["message"] == 'The resource is being rate limited.' or 'You are being rate limited.':
-                print('Rate Limited: ' + code)
+                print(f"Invalid: {code}")
+            else:
+                print(f'Rate Limited: {code}')
                 print('Waiting For Rate Limit To Go Away...')
                 print(data)
                 time.sleep(int(data['retry_after'])/1000)
-            else:
-                print("Working: " + code)
-                file = open("workedcodes.txt", "a+")
-                file.write("\n" + code)
 
 keep_alive()
 Generator()
